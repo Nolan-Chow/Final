4f6306ca0d3850d2815061999804394a8205dad9
Nolan <nolan.chow446@gmail.com>
2020-12-09 13:30:38 -0800

    display large image if hovered over function added

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,29 @@
 user_input = document.querySelector(".user_input");
 thumb_bar = document.querySelector("#thumb-bar");
-
-user_input.style.display = "none";
+displayed_img = document.querySelector(".displayed-img")
 
 function display_img () {
     let imgs = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
     let titles = ["Baby", "Art", "Flower", "Egypt", "Butterfly"]
     for (let i=0; i<imgs.length; ++i) {
-        let img_item = document.createElement("IMG");
+        let img_item = document.createElement("img");
         img_item.setAttribute("src", `images/${imgs[i]}`);
         img_item.setAttribute("title", titles[i])
-        img_item.setAttribute("height", "80");
         img_item.setAttribute("alt", `image ${i}`);
         thumb_bar.appendChild(img_item)
     }
 }
 
+function hovered_img (e) {
+    if (e.target.tagName === "IMG") {
+        img = e.target
+        displayed_img.setAttribute("src", img.src);
+        displayed_img.setAttribute("title", img.title);
+        displayed_img.setAttribute("alt", img.alt);
+        user_input.removeAttribute("style")
+    }
+}
+
+user_input.style.display = "none";
 display_img()
+thumb_bar.addEventListener("mouseover", hovered_img)
